8980088d2fe2acc5044796c4b1ff8bbe79885a7a
Maddie02 <madlensarkisian@gmail.com>
2020-11-12 16:11:35 +0200

    Setup PostgreSQL as default database

--- mask_detect/mask_detect/settings.py
+++ mask_detect/mask_detect/settings.py
@@ -79,10 +79,23 @@ WSGI_APPLICATION = 'mask_detect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USERNAME'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT')
     }
 }
+
+
+# Default SQLite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation

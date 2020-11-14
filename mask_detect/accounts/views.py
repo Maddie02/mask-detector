5e727ab841b1394e6e2c77838dae30df2145965b
Maddie02 <madlensarkisian@gmail.com>
2020-11-14 18:15:44 +0200

    [New] Add basic templates

--- mask_detect/accounts/views.py
+++ mask_detect/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+def register(request):
+    return render(request, 'accounts/register.html')
--- mask_detect/mask_detect/urls.py
+++ mask_detect/mask_detect/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', accounts_views.home, name="home"),
+    path('register/', accounts_views.register, name="register"),
 ]

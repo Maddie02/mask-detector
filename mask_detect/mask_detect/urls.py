42b2eea7bcc011632a6d4fa551ce519a8fa01e82
Maddie02 <madlensarkisian@gmail.com>
2020-11-16 20:52:29 +0200

    [New] Added login user functionality

--- mask_detect/mask_detect/urls.py
+++ mask_detect/mask_detect/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from accounts import views as accounts_views
 
@@ -21,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', accounts_views.home, name="home"),
     path('register/', accounts_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name="logout")
 ]

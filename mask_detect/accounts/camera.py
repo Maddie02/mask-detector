ff3b5b61b1eb768f8270761820f74c1f7672d12a
Maddie02 <madlensarkisian@gmail.com>
2020-12-08 21:32:12 +0200

    [New] Created functions for loading face detector and face mask detector models

---
+++ mask_detect/accounts/camera.py
@@ -0,0 +1,36 @@
+import cv2, os
+from keras.models import load_model
+
+current_path = os.path.abspath(".")
+path_to_model = os.path.join(current_path, "../ai/", "face-mask-detector.h5")
+
+
+def load_face_detector():
+    print("Loading face detector model...")
+    try:
+        prototxt_file_path = os.path.join(current_path, "accounts/face-detect", "deploy.prototxt.txt")
+        weights_path =  os.path.join(current_path, "accounts/face-detect", "res10_300x300_ssd_iter_140000.caffemodel")
+        print("Face detector model succesfully loaded!")
+        
+    except Exception as e:
+        print("Error while loading face detector pre-trained model.", e)
+
+    face_net = cv2.dnn.readNet(prototxt_file_path, weights_path)
+    return face_net
+
+def load_face_mask_detector():
+
+    print("Loading face mask detector model...")
+
+    try:
+        model = load_model(path_to_model)
+    
+    except Exception as e:
+        print("Error while loading face mask detector.", e)
+    
+    return model
+
+
+
+
+

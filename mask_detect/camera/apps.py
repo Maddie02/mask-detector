2c66e770b314eb4da08024f9c334e493c6acf693
Maddie02 <madlensarkisian@gmail.com>
2021-01-25 21:01:00 +0200

    [Update] Removed the ready function

--- mask_detect/camera/apps.py
+++ mask_detect/camera/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 from threading import Thread
-from camera.views import CameraThread
 
 
 class CameraConfig(AppConfig):
     name = 'camera'
-
-    def ready(self):
-        CameraThread().start()

0625980000feeb79db17a93e7c453b10b4a1f9bd
Maddie02 <madlensarkisian@gmail.com>
2021-01-28 23:09:34 +0200

    [Update] Configure SMTP settings

--- mask_detect/mask_detect/settings.py
+++ mask_detect/mask_detect/settings.py
@@ -150,3 +150,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False

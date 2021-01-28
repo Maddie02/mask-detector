1998bb33f44fb59bc2a74802212cc81f78d7e5f5
Maddie02 <madlensarkisian@gmail.com>
2021-01-29 00:41:51 +0200

    [Update] Send an email to user when he's not wearing a mask

--- mask_detect/camera/controller.py
+++ mask_detect/camera/controller.py
@@ -3,7 +3,10 @@ from camera.camera import Camera
 from threading import Thread
 from django.core.mail import send_mail
 from mask_detect.settings import EMAIL_HOST_USER
+from datetime import datetime
 import time
+
+WAIT_MINUTES = 5.0
 
 class CameraThread(Thread):
 
@@ -17,6 +20,8 @@ class CameraThread(Thread):
 
 
 def run_camera(user, camera):
+    last_seen = 0
+
     while camera.video.isOpened():
         success, frame = camera.get_frame()
 
@@ -34,8 +39,21 @@ def run_camera(user, camera):
             
             # Send email to user and do other stuff if he's not wearing a mask
             if label == "No Mask":
-                pass
+                employee_name = user.first_name + ' ' + user.last_name
 
+                if last_seen == 0:
+                    last_seen = datetime.now()
+                    send_alert_mail(employee_name, user.email, last_seen)
+                    continue
+                
+                time_interval = datetime.now() - last_seen
+
+                if time_interval.total_seconds() / 60 >= WAIT_MINUTES:
+                    last_seen = datetime.now()
+                    send_alert_mail(employee_name, user.email, last_seen)
+            else:
+                last_seen = 0
+            
             label = f'{label}: {(max(mask, without_mask) * 100):.2f}%'
 
             cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
@@ -50,11 +68,36 @@ def run_camera(user, camera):
     camera.release()
 
 
-def send_alert_mail(email):
+def send_alert_mail(name, email, last_seen):
+
+    message = f'''
+        Hello, {name},
+        \n\n
+        The Mask Detector caught you not wearing a mask at {last_seen.strftime("%H:%M:%S, %d/%m/%Y")}. 
+        \n\n
+        Please, wear your mask and stay safe!
+        \n\n
+        With love,\n
+        The Mask Detector Team
+    '''
+
+    html_message = f'''
+        Hello, {name},
+        <br> <br>
+        The Mask Detector caught you not wearing a mask at {last_seen.strftime("%H:%M:%S, %d/%m/%Y")}. 
+        <br> <br>
+        <strong>Please, wear your mask and stay safe!</strong>
+        <br> <br>
+        With love,<br>
+        The Mask Detector Team
+    '''
+
     send_mail(
         "Mask Detector Alert",
-        "The Mask Detector caught you not wearing a mask. Please wear your mask and be safe!",
-            "Mask Detector",
-            [email],
-            fail_silently=False
+        message,
+        "Mask Detector",
+        [email],
+        fail_silently=False,
+        html_message=html_message
     )
+

0c73f3acb807da75f50edfc69a2cb8a676adad3f
Maddie02 <madlensarkisian@gmail.com>
2021-02-04 21:51:25 +0200

    [New] Register statistics

--- mask_detect/accounts/admin.py
+++ mask_detect/accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Company, Employee
+from .models import Company, Employee, Statistic
 from .forms import SignUpEmployeeForm
 from django.contrib.auth.admin import UserAdmin
 
@@ -17,3 +17,4 @@ class EmployeeUserAdmin(UserAdmin):
 
 admin.site.register(Company)
 admin.site.register(Employee, EmployeeUserAdmin)
+admin.site.register(Statistic)

70d5219fe7c805b9762a8fb3fd401969e408aa2b
Maddie02 <madlensarkisian@gmail.com>
2021-02-07 22:50:10 +0200

    [New] Created database UML diagram

--- mask_detect/mask_detect/settings.py
+++ mask_detect/mask_detect/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-
+    'django_extensions',
     'accounts.apps.AccountsConfig',
     'camera.apps.CameraConfig',
     'stats.apps.StatsConfig'

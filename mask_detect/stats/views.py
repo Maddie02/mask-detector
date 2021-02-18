c9ea6ab10ecb5eadb0e6d21bcdba097717f2d8d4
Maddie02 <madlensarkisian@gmail.com>
2021-02-18 20:00:36 +0200

    [Update] Show statistics for different companies

--- mask_detect/stats/views.py
+++ mask_detect/stats/views.py
@@ -3,7 +3,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Statistic
-from accounts.models import Employee
+from accounts.models import Employee, Company
 from django.http import HttpResponse
 import plotly.graph_objects as go
 import plotly.offline as opy
@@ -11,14 +11,13 @@ import datetime
 import csv
 
 
-def get_employee_stats(stats):
+def get_employee_stats(stats, company):
 
     stats_dict = {}
 
     for stat in stats:
         stats_dict[stat.employee.first_name + ' ' + stat.employee.last_name] = stat.count_violations
     
-    print(stats_dict)
     employees = list(stats_dict.keys())
     violations = list(stats_dict.values())
 
@@ -35,7 +34,7 @@ def get_employee_stats(stats):
             )]
 
     layout = go.Layout(
-        title="Violations of all employees",
+        title="Violations of all employees" if company == None else f"Violations in {company}",
         font=dict(
             family='Poppins, monospace',
             size=14,
@@ -84,7 +83,13 @@ def dashboard(request):
 
     disable = 'disabled' if len(stats) == 0 else None
 
-    return render(request, 'accounts/dashboard.html', {'employees': employees, 'stats': stats, 'disable': disable})
+    context = {
+        'employees': employees,
+        'stats': stats,
+        'disable': disable
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 @staff_member_required
@@ -115,7 +120,19 @@ def delete_dashboard_stats(request):
 @staff_member_required
 def view_charts(request):
 
+    companies = Company.objects.all()
     stats = Statistic.objects.all()
 
-    return render(request, 'accounts/charts.html', {'chart': get_employee_stats(stats)})
+    charts_for_company = []
+
+    for company in companies:
+        stats_per_company = Statistic.objects.filter(employee__company__name=company)
+        charts_for_company.append(get_employee_stats(stats_per_company, company=company))
+
+    context = {
+        'chart': get_employee_stats(stats, company=None),
+        'charts_for_company': charts_for_company,
+    }
+
+    return render(request, 'accounts/charts.html', context)
 

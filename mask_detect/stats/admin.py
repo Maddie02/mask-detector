35907b4073d3759adfaf1776d1f38d59799c2b17
Maddie02 <madlensarkisian@gmail.com>
2021-03-02 16:51:55 +0200

    [Update] Created Violation model

--- mask_detect/stats/admin.py
+++ mask_detect/stats/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Statistic
+from .models import Statistic, Violation
 
 admin.site.register(Statistic)
+admin.site.register(Violation)
--- mask_detect/stats/models.py
+++ mask_detect/stats/models.py
@@ -3,9 +3,16 @@ from accounts.models import Employee
 
 class Statistic(models.Model):
     employee = models.OneToOneField(Employee, on_delete=models.CASCADE, null=True)
-    count_violations = models.IntegerField(default=0, null=True)
-    last_seen_date = models.DateTimeField(null=True)
-    
+
     def __str__(self):
-        return f'{self.employee.first_name} {self.employee.last_name}'
+        return f'Statistic - {self.employee.first_name} {self.employee.last_name}'
+
+
+class Violation(models.Model):
+    violation_date = models.DateTimeField()
+    frame = models.ImageField(upload_to='violations/', blank=True, null=True)
+    statistic = models.ForeignKey(Statistic, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Violation - {self.statistic.employee.first_name} {self.statistic.employee.last_name}'
 

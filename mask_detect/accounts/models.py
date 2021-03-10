019ad7363a85845f46e215495a0422473600c1c2
Maddie02 <madlensarkisian@gmail.com>
2021-03-10 17:07:28 +0200

    [Update] Delete profile pic from server when updated

--- mask_detect/accounts/models.py
+++ mask_detect/accounts/models.py
@@ -21,4 +21,23 @@ class Employee(AbstractUser):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
+
+    def remove_on_image_update(self):
+        try:
+            employee = Employee.objects.get(id=self.id)
+        except:
+            return
+        
+        if employee.profile_pic and self.profile_pic and employee.profile_pic != self.profile_pic:
+            employee.profile_pic.delete()
+        
+    def delete(self, *args, **kwargs):
+        self.profile_pic.delete()
+        return super(Employee, self).delete(*args, *kwargs)
+    
+    def save(self, *args, **kwargs):
+        self.remove_on_image_update()
+        return super(Employee, self).save(*args, **kwargs)
+
+
     
